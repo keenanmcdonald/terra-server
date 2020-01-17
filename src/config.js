d8aaf3e6a7e9d74a5660a00efbc1b9d8e115ced5
Keenan McDonald <keemcd@gmail.com>
2020-01-16 23:05:38 -0600

    replace db_url with database_url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '30d',
-    DB_URL: process.env.DB_URL || 'postgresql://localhost/terra',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://localhost/terra',
 }
