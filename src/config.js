19aa6b6ce34190bdcb4fd52cd257b7be41581700
Keenan McDonald <keemcd@gmail.com>
2020-01-17 11:02:59 -0600

    fixed port in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.eventNames.PORT || 8000,
+    PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '30d',
